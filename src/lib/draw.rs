5973a8e935d787268dd55ca4044d69aa92310fd4
lulucoco <chattyu@outlook.com>
2023-11-28 20:19:31 -0500

    add 1 more function

--- src/lib/draw.rs
+++ src/lib/draw.rs
@@ -4,6 +4,21 @@ The original C code was used for drawing primitives like lines on an SDL surface
 Here, we have translated and adapted a part of that functionality into Rust,
 using the SDL2 crate for handling graphics in Rust.
 */
+fn add_pixel_to_drawn_list(x: i32, y: i32, pts: &mut [i32; 4]) {
+    if x < pts[0] {
+        pts[0] = x;
+    }
+    if y < pts[1] {
+        pts[1] = y;
+    }
+    if x > pts[2] {
+        pts[2] = x;
+    }
+    if y > pts[3] {
+        pts[3] = y;
+    }
+}
+
 
 fn add_pixel_to_drawn_list(x: i32, y: i32, pts: &mut [i32; 4]) {
     if x < pts[0] {
@@ -35,3 +50,5 @@ fn add_line_to_drawn_list(x1: i32, y1: i32, x2: i32, pts: &mut [i32; 4]) {
         pts[3] = y2;
     }
 }
+
+
